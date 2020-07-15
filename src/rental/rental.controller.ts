62011244969dc57fd318b80f5044af93a3650b1f
ade <ademorypro@gmail.com>
2020-07-15 14:53:53 +0200

    Corrected rental path

--- src/rental/rental.controller.ts
+++ src/rental/rental.controller.ts
@@ -2,16 +2,16 @@ import { Controller, Post, Body, Get } from '@nestjs/common';
 import { Rental } from './rental.entity';
 import { RentalService } from './rental.service';
 
-@Controller()
+@Controller('rental')
 export class RentalController {
     constructor(private readonly rentalService: RentalService) { }
 
-    @Get('rental')
+    @Get()
     getRental(): Promise<Rental[]> {
         return this.rentalService.get();
     }
 
-    @Post('rental')
+    @Post()
     createRental(@Body() rental: Rental): Promise<Rental> {
         return this.rentalService.create(rental);
     }
